6feb710c4f030f54205a019926eae9f3532953de
LironSif <lironsifado@gmail.com>
2024-01-07 00:02:02 +0200

    get user by email

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,6 +12,20 @@ export const getAllUsers = async (req,res,next) => {
     }
 }
 
+export const getUserByEmail = async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.params.email });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+};
+
+ 
+
 // Controller to get single user
 export const getUserById = async (req,res,next) => {
     try {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { getAllUsers, getUserById, createUser} from "../controllers/userController.js";
+import { getUserByEmail,getAllUsers, getUserById, createUser} from "../controllers/userController.js";
 
 const router = express.Router()
 
 router.get('/', getAllUsers)
 router.get('/:id', getUserById)
 router.post('/', createUser)
-
+router.get('/email/:email', getUserByEmail);
 
 export default router
